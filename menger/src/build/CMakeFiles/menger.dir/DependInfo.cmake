b2d719f6149881cd6ac932eb1f721805cb41f990
doug <d.archibald@gmail.com>
2020-03-02 16:09:28 -0600

    Initial commit

---
+++ menger/src/build/CMakeFiles/menger.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/doug/Storage/CS354/hw3-menger/menger/src/camera.cc" "/home/doug/Storage/CS354/hw3-menger/menger/src/build/CMakeFiles/menger.dir/camera.o"
+  "/home/doug/Storage/CS354/hw3-menger/menger/src/debuggl.cc" "/home/doug/Storage/CS354/hw3-menger/menger/src/build/CMakeFiles/menger.dir/debuggl.o"
+  "/home/doug/Storage/CS354/hw3-menger/menger/src/main.cc" "/home/doug/Storage/CS354/hw3-menger/menger/src/build/CMakeFiles/menger.dir/main.o"
+  "/home/doug/Storage/CS354/hw3-menger/menger/src/menger.cc" "/home/doug/Storage/CS354/hw3-menger/menger/src/build/CMakeFiles/menger.dir/menger.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ menger/src/build/CMakeFiles/menger.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/menger.dir/camera.o"
+  "CMakeFiles/menger.dir/debuggl.o"
+  "CMakeFiles/menger.dir/main.o"
+  "CMakeFiles/menger.dir/menger.o"
+  "menger.pdb"
+  "menger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/menger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
